254ce11929cbbc8fa06287150929501c22245fab
ravidMAGS <ravid.aharon@mags.school.nz>
2025-10-08 09:17:56 +1300

    add memoisation

--- CoinChange.py
+++ CoinChange.py
@@ -1,7 +1,14 @@
+memo = {}
+
 def count_coins(sum, index = 0):
+    if (sum, index) in memo:
+        return memo[(sum, index)]
+
     if sum == 0:
         return 1
     if sum < 0 or index >= len(coins):
         return 0
 
-    return count_coins(sum - coins[index], index) + count_coins(sum, index + 1)
+    memo[(sum,index)] = count_coins(sum - coins[index], index) + count_coins(sum, index + 1)
+    return memo[(sum,index)]
+
